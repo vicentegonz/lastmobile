408242c348cda54961acd22a34d097004c424e1f
Daniel Leal <37158127+daleal@users.noreply.github.com>
2021-05-13 23:17:33 -0400

    Update URLs (#21)
    * Update staging base URL
    
    * Update production URL

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-export const STAGING_BASE_URL = 'https://staging-api.capstone.com';
-export const PRODUCTION_BASE_URL = 'https://api.capstone.com';
+export const STAGING_BASE_URL = 'https://api.staging-arcopri.ml';
+export const PRODUCTION_BASE_URL = 'https://api.arcopri.ml';
